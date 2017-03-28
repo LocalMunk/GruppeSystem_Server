1bf29967d7595dbebe0bd4cf29988009e9c9ab14
Martin Dalby <martindalby@live.dk>
2017-03-28 12:15:13 +0200

    Init

---
+++ src/gruppesystem_server/GruppeSystem_Server.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppesystem_server;
+
+/**
+ *
+ * @author elbosso
+ */
+public class GruppeSystem_Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
